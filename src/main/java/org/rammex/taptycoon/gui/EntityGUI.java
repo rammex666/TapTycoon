64e643c79eea9efcd1d324af91c21f235e0aadef
rammex <romain.kaspar@gmail.com>
2024-05-23 09:37:07 +0200

    gui

---
+++ src/main/java/org/rammex/taptycoon/gui/EntityGUI.java
@@ -0,0 +1,20 @@
+package org.rammex.taptycoon.gui;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.rammex.taptycoon.utils.ItemBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EntityGUI {
+
+    public void getInventory(Player player){
+        Inventory inv = Bukkit.createInventory(null, 27, "TapTycoon Manager")
+
+
+    }
+
+}
